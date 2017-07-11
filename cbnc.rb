7e83dac8d2ed2cf75456aa2fea605ee353356580
bolyardk22 <collinsk22@mycu.concord.edu>
2017-07-11 13:52:21 -0400

    added a gets.chomp

--- cbnc.rb
+++ cbnc.rb
@@ -1,3 +1,8 @@
+def what_number
+	p "Please enter a 4-digit number."
+	ticket = gets.chomp
+end
+
 def lotto_func(n)
 	winners = ["3748","8343","1232","8524","5242"]
 	winners.include?"#{n}"
@@ -10,3 +15,5 @@ def lucky(x)
 		a = "Sorry, you lost this time."
 	end
 end
+
+puts lucky(lotto_func(what_number))
